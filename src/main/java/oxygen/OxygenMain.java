8a2c2476b00bab937e687c04be28642b75e2a174
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-06-29 01:43:15 +0300

    Working on JavaDoc
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- src/main/java/oxygen/OxygenMain.java
+++ src/main/java/oxygen/OxygenMain.java
@@ -56,9 +56,9 @@ public class OxygenMain {
     private static final String PATH_TO_JSON = "../nfL6.json";
     private static final String PATH_TO_INDEX1 = "./indexes/index";
     private static final String PATH_TO_INDEX2 = "./indexes/shingle_index";
-    private static final String PATH_TO_QUESTIONS = "./test/questions2.txt";
+    private static final String PATH_TO_QUESTIONS = "./test/finalEval.txt";
     private static final String PATH_TO_QUESTIONS_MAIN = "./test/questions.txt";
-    private static final String PATH_TO_ANSWERS_OUTPUT = "./test/out/answers2.json";
+    private static final String PATH_TO_ANSWERS_OUTPUT = "./test/out/answers.json";
     private static final float DEFAULT_LAMBDA;
 
     private static final String BODY_FIELD = "body";
